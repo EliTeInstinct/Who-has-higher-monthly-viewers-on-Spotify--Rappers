9ac03658c70e047a4d1a92cc88cbd243ed809317
Rxvera7 <wwee31el9@gmail.com>
2024-11-12 20:03:31 -0800

    Add files via upload

---
+++ lab-11-GUI-ex-01.py
@@ -0,0 +1,10 @@
+#Jeffrey Ramirez, Arturo Rivera, Christian Zarseno Hernandez, Yener Lopez 
+
+import tkinter
+game = tkinter.Tk()
+game.geometry("900x900")
+game.configure(bg='black')
+game.title('My app')
+
+game.mainloop()
+
